8b0e459aaf2e299445c1295a8f41d21fee3f68c3
sheeb <seamushinz@icloud.com>
2025-01-25 17:57:25 -0500

    added info for facial recognition testing

--- stress-app/src/app/page.js
+++ stress-app/src/app/page.js
@@ -6,8 +6,6 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1 className={styles.title}>Stress Test</h1>
-        <p className={styles.description}>This is a stress test application.</p>
         <Webcam/>
         </main>
     </div>
--- stress-app/src/components/webcam.jsx
+++ stress-app/src/components/webcam.jsx
@@ -1,99 +1,102 @@
 "use client"
 import { useEffect, useState } from 'react';
 import styles from '../app/page.module.css';
-import { FilesetResolver, FaceLandmarker, DrawingUtils } from "@mediapipe/tasks-vision";
+import { FilesetResolver, FaceLandmarker } from "@mediapipe/tasks-vision";
 
 export default function Webcam() {
-    const [blendShapes, setBlendShapes] = useState([]);
+   const [blendShapes, setBlendShapes] = useState([]);
 
-    useEffect(() => {
-        const video = document.getElementById("webcam");
-        const canvasElement = document.getElementById("output_canvas");
-        const canvasCtx = canvasElement.getContext("2d");
+   useEffect(() => {
+       const video = document.getElementById("webcam");
+       const constraints = {
+           audio: false,
+           video: true,
+       };
 
-        const constraints = {
-            audio: false,
-            video: true,
-        };
+       navigator.mediaDevices
+           .getUserMedia(constraints)
+           .then((stream) => {
+               const videoTracks = stream.getVideoTracks();
+               console.log("Got stream with constraints:", constraints);
+               console.log(`Using video device: ${videoTracks[0].label}`);
+               stream.onremovetrack = () => {
+                   console.log("Stream ended");
+               };
+               video.srcObject = stream;
+               video.onloadedmetadata = () => {
+                   video.play();
+                   createTask();
+               };
+           })
+           .catch((error) => {
+               if (error.name === "OverconstrainedError") {
+                   console.error(
+                       `The resolution ${constraints.video.width.exact}x${constraints.video.height.exact} px is not supported by your device.`,
+                   );
+               } else if (error.name === "NotAllowedError") {
+                   console.error(
+                       "You need to grant this page permission to access your camera and microphone.",
+                   );
+               } else {
+                   console.error(`getUserMedia error: ${error.name}`, error);
+               }
+           });
 
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then((stream) => {
-                const videoTracks = stream.getVideoTracks();
-                console.log("Got stream with constraints:", constraints);
-                console.log(`Using video device: ${videoTracks[0].label}`);
-                stream.onremovetrack = () => {
-                    console.log("Stream ended");
-                };
-                video.srcObject = stream;
-            })
-            .catch((error) => {
-                if (error.name === "OverconstrainedError") {
-                    console.error(
-                        `The resolution ${constraints.video.width.exact}x${constraints.video.height.exact} px is not supported by your device.`,
-                    );
-                } else if (error.name === "NotAllowedError") {
-                    console.error(
-                        "You need to grant this page permission to access your camera and microphone.",
-                    );
-                } else {
-                    console.error(`getUserMedia error: ${error.name}`, error);
-                }
-            });
+       async function createTask() {
+           const vision = await FilesetResolver.forVisionTasks(
+               "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+           );
+           const faceLandmarker = await FaceLandmarker.createFromOptions(
+               vision,
+               {
+                   baseOptions: {
+                       modelAssetPath: "/models/face_landmarker.task"
+                   },
+                   runningMode: 'VIDEO',
+                   outputFaceBlendshapes: true,
+                   outputFaceLightshapes: true
+               }
+           );
 
-        async function createTask() {
-            const vision = await FilesetResolver.forVisionTasks(
-                "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
-            );
-            const faceLandmarker = await FaceLandmarker.createFromOptions(
-                vision,
-                {
-                    baseOptions: {
-                        modelAssetPath: "/public/models/blaze_face_short_range.tflite"
-                    },
-                    runningMode: 'VIDEO',
-                    outputFaceBlendshapes: true,
-                    outputFaceLightshapes: true
-                }
-            );
+           await faceLandmarker.setOptions({ runningMode: "VIDEO" });
 
-            await faceLandmarker.setOptions({ runningMode: "VIDEO" });
+           let lastVideoTime = -1;
 
-            let lastVideoTime = -1;
-            const drawingUtils = new DrawingUtils(canvasCtx);
+           function renderLoop() {
+               if (video.currentTime !== lastVideoTime) {
+                   const faceLandmarkerResult = faceLandmarker.detectForVideo(video, performance.now());
+                   if (faceLandmarkerResult.faceLandmarks) {
+                       if (faceLandmarkerResult.faceBlendshapes) {
+                           console.log("Blendshapes detected:", faceLandmarkerResult.faceBlendshapes[0].categories);
+                           setBlendShapes(faceLandmarkerResult.faceBlendshapes[0].categories);
+                       }
+                       lastVideoTime = video.currentTime;
+                   }
+               }
+               requestAnimationFrame(renderLoop);
+           }
 
-            function renderLoop() {
-                if (video.currentTime !== lastVideoTime) {
-                    const faceLandmarkerResult = faceLandmarker.detectForVideo(video, performance.now());
-                    if (faceLandmarkerResult.faceLandmarks) {
-                        if (faceLandmarkerResult.faceBlendshapes) {
-                            setBlendShapes(faceLandmarkerResult.faceBlendshapes[0].categories);
-                        }
-                        lastVideoTime = video.currentTime;
-                    }
-                }
-                requestAnimationFrame(renderLoop);
-            }
+           renderLoop();
+       }
+   }, []);
 
-            renderLoop();
-        }
-
-        createTask();
-    }, []);
-
-    return (
-        <div className={styles.container}>
-            <h2 className={styles.title}>Webcam</h2>
-            <p className={styles.description}>This is a webcam test.</p>
-            <video id="webcam" autoPlay playsInline muted></video>
-            <canvas className="output_canvas" id="output_canvas"></canvas>
-            <ul>
-                {blendShapes.map((shape, index) => (
-                    <li key={index}>
-                        {shape.displayName || shape.categoryName}: {shape.score.toFixed(4)}
-                    </li>
-                ))}
-            </ul>
+   return (
+<div className={styles.container}>
+    <h2 className={styles.title}>Webcam</h2>
+    <p className={styles.description}>This is a webcam test.</p>
+    <div style={{ display: 'flex' }}>
+        <div>
+            <video id="webcam" autoPlay playsInline muted width="640" height="480"></video>
+            <canvas className="output_canvas" id="output_canvas" width="640" height="480"></canvas>
         </div>
-    );
+        <ul style={{ marginRight: '150px' }}>
+            {blendShapes.map((shape, index) => (
+                <li key={index}>
+                    {shape.displayName || shape.categoryName}: {shape.score.toFixed(4)}
+                </li>
+            ))}
+        </ul>
+    </div>
+</div>
+   );
 }
