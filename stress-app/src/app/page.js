78175b7d73b6dc69b4efc73d2aeb17dd925dc7f5
sheeb <seamushinz@icloud.com>
2025-01-25 18:50:26 -0500

    fixed imports in page.js

--- stress-app/src/app/page.js
+++ stress-app/src/app/page.js
@@ -1,14 +1,13 @@
-"use client"
-    import styles from "./page.module.css";
+ import styles from "./page.module.css";
     import Webcam from "@/components/webcam";
     import NotificationRequestButton from "@/components/notificationRequestButton";
-    import { useEffect } from "react";
 
     export default function Home() {
       return (
         <div className={styles.page}>
           <main className={styles.main}>
             <NotificationRequestButton/>
+            <Webcam />
           </main>
         </div>
       );
