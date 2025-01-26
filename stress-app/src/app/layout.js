d13f24b8f5b1f54052b357b4dc475aba3234a6e6
ColinClark383 <colin.clark@ufl.edu>
2025-01-26 02:46:05 -0500

    layout edits

--- stress-app/src/app/layout.js
+++ stress-app/src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" sizes="any"/>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
